e9c7225c4781f68a6d95735a48d2623b26f1fe8c
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-03-22 16:14:24 +0600

    Inventory Management

---
+++ resources/js/Pages/HomePage.vue
@@ -0,0 +1,12 @@
+<script setup>
+ import { Link } from '@inertiajs/vue3';
+</script>
+ 
+<template>
+<h1>Home page</h1>
+<Link href="/test">Test Page</Link>
+</template>
+ 
+<style scoped>
+ 
+</style>
